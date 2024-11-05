847fe7cdac45cb6f8608931b24c5f55a627f13f3
Justine Mae Macario <149843701+bongakcsstudent@users.noreply.github.com>
2024-11-05 23:21:45 +0800

    Add files via upload
    first part, theres a lot of data structures

---
+++ dataStructuresStudy.py
@@ -0,0 +1,26 @@
+#Rediscovered my Leetcode acc, literally what OMG, part1 of data structures, I'm reviewing leetcode questions
+List = ["Bro", "is", "rediscovering", "what", "python", "is"]
+print(List)
+
+Dictionary = {"Demon's": "Souls",
+              "Dark" : "Souls 1",
+              "Dark" : "Souls 2",
+              "Dark" : "Souls 3",
+              "Blood" : "Borne",
+              "Se" : "Kiro",
+              "Elden" : "Ring"
+              }
+
+print(Dictionary)
+
+tu_ple = ["I", "like", "information", "management", "so", "I'll", "try", "to", "perfect", "every", "activity"]
+cc5 = tuple(tu_ple)
+print(cc5)
+
+what_are_sets = ["palakol", 75, "ang", "aking" "grades", "abutin", "ang", 85, "!"]
+freezing_the_set = frozenset(what_are_sets)
+print(freezing_the_set)
+
+string = "sinulid"
+print(string[2])
+
