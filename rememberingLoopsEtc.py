9793ce86ec8cc775fade6b963e1af4e232a5ebed
Justine Mae Macario <149843701+bongakcsstudent@users.noreply.github.com>
2024-11-06 23:28:13 +0800

    Add files via upload
    I HAVE A CONYO BGC BRO WHEN I REMEMBER ALL THOSE CC2 CONCEPTS ITS CRAZY I SAY.

---
+++ rememberingLoopsEtc.py
@@ -0,0 +1,22 @@
+my_grade = 76
+
+if my_grade > 75:
+    print("man pasado, anong klaseng Python Code to bat pambata bro, give me LeetCode")
+else:
+    print("tangina bro, bagsak ako bro, lmao bro")
+
+time = 0
+while (time < 20):
+    time = time + 1
+    print("anong klaseng loop to gar.")
+else :
+    print("tapos na tong loop mo bro")
+
+word = "Isang Salita"
+for i in word:
+    print(i)
+
+print("GRABENG PRACTICE BRO HOOO")
+
+
+
